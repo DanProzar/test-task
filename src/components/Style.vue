ec29e912a75b4ce8a25102942335f76404fd2d5a
WebMaster <prozardan@gmail.com>
2019-09-15 15:20:51 +0300

    Setup the project

---
+++ src/components/Style.vue
@@ -0,0 +1,36 @@
+<style>
+  @font-face {
+  font-family: 'Overpass';
+  src: url('../assets/fonts/Overpass/Overpass-SemiBold.eot?#iefix') format('embedded-opentype'),
+  url('../assets/fonts/Overpass/Overpass-SemiBold.woff') format('woff'),
+  url('../assets/fonts/Overpass/Overpass-SemiBold.ttf') format('truetype'),
+  url('../assets/fonts/Overpass/Overpass-SemiBold.svg#Overpass-SemiBold') format('svg');
+  font-weight: 600;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'SourceSansPro';
+  src: url('../assets/fonts/SourceSansPro/Bold/SourceSansPro-Bold.eot?#iefix') format('embedded-opentype'),
+  url('../assets/fonts/SourceSansPro/Bold/SourceSansPro-Bold.woff') format('woff'),
+  url('../assets/fonts/SourceSansPro/Bold/SourceSansPro-Bold.ttf') format('truetype'),
+  url('../assets/fonts/SourceSansPro/Bold/SourceSansPro-Bold.svg#SourceSansPro-Bold') format('svg');
+  font-weight: bold;
+  font-style: normal;
+}
+@font-face {
+  font-family: 'SourceSansPro';
+  src: url('../assets/fonts/SourceSansPro/Regular/SourceSansPro-Regular.eot?#iefix') format('embedded-opentype'),
+  url('../assets/fonts/SourceSansPro/Regular/SourceSansPro-Regular.woff') format('woff'),
+  url('../assets/fonts/SourceSansPro/Regular/SourceSansPro-Regular.ttf') format('truetype'),
+  url('../assets/fonts/SourceSansPro/Regular/SourceSansPro-Regular.svg#SourceSansPro-Regular') format('svg');
+  font-weight: normal;
+  font-style: normal;
+}
+
+body {
+  font-size: 16px;
+  font-family: 'SourceSansPro', Arial;
+}
+
+</style>
